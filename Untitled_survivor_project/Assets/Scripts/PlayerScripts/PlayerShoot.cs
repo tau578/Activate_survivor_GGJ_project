f34e5a8ea0ff05d444fe80e43969ac87c9dce81a
parkourtales9-blip <parkourtales9@gmail.com>
2026-01-31 16:14:39 +0100

    shooting fixes

--- Untitled_survivor_project/Assets/Scripts/PlayerScripts/PlayerShoot.cs
+++ Untitled_survivor_project/Assets/Scripts/PlayerScripts/PlayerShoot.cs
@@ -11,6 +11,9 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip[] gunShots;
 
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float maxShootDistance = 1000f;
+
 
     private float fireRaateTimer = 0f;
     private void Update()
@@ -24,15 +27,33 @@ public class PlayerShoot : MonoBehaviour
     }
     private void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Vector3 targetPoint;
+
+        // Ray from center of screen
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, maxShootDistance))
+        {
+            targetPoint = hit.point; // shoot where we hit
+        }
+        else
+        {
+            targetPoint = ray.origin + ray.direction * maxShootDistance;
+        }
+
+        // Direction from gun to target
+        Vector3 direction = (targetPoint - firePoint.position).normalized;
+
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.LookRotation(direction));
+
         Bullet bulletScript = bullet.GetComponent<Bullet>();
         if (bulletScript != null)
         {
-            bulletScript.MoveButllet(firePoint.forward, bulletSpeed);
+            bulletScript.MoveButllet(direction, bulletSpeed);
         }
-       // Destroy(bullet, 2f); // Destroy the bullet after 2 seconds to avoid clutter
 
-       PlayRandomShot();
+        PlayRandomShot();
     }
 
     private void PlayRandomShot()
