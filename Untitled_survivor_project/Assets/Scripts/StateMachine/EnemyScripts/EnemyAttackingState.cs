99c434696ffabdef910f5f45bc059c33a3c9a6d3
Shervin Shafizad <shervinshafizadentertainment@gmail.com>
2026-01-31 14:35:40 +0100

    Enemy deals damage

--- Untitled_survivor_project/Assets/Scripts/StateMachine/EnemyScripts/EnemyAttackingState.cs
+++ Untitled_survivor_project/Assets/Scripts/StateMachine/EnemyScripts/EnemyAttackingState.cs
@@ -11,6 +11,7 @@ public class EnemyAttackingState : EnemyBaseState
     public override void Enter()
     {
         stateMachine.Animator.CrossFadeInFixedTime(punchHash, 0f);
+        Debug.Log("entered attacking state");
        // stateMachine.PunchWarningBox.gameObject.SetActive(true); for later if we want to change attack logic 
         //EnemyPunchWarningDetector detector = stateMachine.Player.GetComponentInChildren<EnemyPunchWarningDetector>();
      //   if(detector != null)
@@ -21,10 +22,12 @@ public class EnemyAttackingState : EnemyBaseState
     }
     public override void Tick(float deltaTime)
     {
+        Debug.Log("attacking state ticking");
         timer += deltaTime;
-        if(timer >=  0.6f && ! hasAlreadyDealtDamge && IsInAttackRange()) 
+        if(timer >=  0.6f && IsInAttackRange() && !hasAlreadyDealtDamge) //
         {
             // stateMachine.PunchHitBox.gameObject.SetActive(true); for later if we want to change attack logic 
+            Debug.Log("Enemy dealt damage to player");
             stateMachine.Player.GetComponent<Health>().DealDamage(1);
             hasAlreadyDealtDamge = true;
             stateMachine.didRecentlyPunchPlayer = true;
